a64b73445e22e74784cc0afe11bfcfd27bc98d75
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-03-30 22:52:08 +0200

    add sim.tcl

---
+++ sim.tcl
@@ -0,0 +1,24 @@
+#!/bin/sh
+# \
+exec vivado -mode gui -source "$0" -tclargs "$@"
+
+create_project -in_memory
+
+read_vhdl "util.vhd"
+foreach { file } [ glob -directory "util/" -- "*" ] {
+    read_vhdl "$file"
+}
+
+foreach { file } [ glob -directory "tb/" -- "*" ] {
+    add_files -fileset sim_1 "$file"
+}
+
+save_project_as -force project_tb project_tb
+
+proc sim { tb } {
+    set_property top_lib xil_defaultlib [ get_filesets sim_1 ]
+    set_property top $tb [ get_filesets sim_1 ]
+    launch_simulation
+}
+
+sim tb_half_adder
