719d9b740f37c486f67297697032902d9192655f
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-04-05 11:41:04 +0200

    [tb_ram] fix pars

--- tb/tb_ram.vhd
+++ tb/tb_ram.vhd
@@ -12,11 +12,11 @@ architecture arch of tb_ram is
 
     constant clk_period : time := 10 ns;
 
-    constant W : integer := 4;
+    constant W : integer := 16;
     constant N : integer := 4;
 
     signal clk : std_logic := '0';
-    signal address : std_logic_vector(W-1 downto 0);
+    signal address : std_logic_vector(N-1 downto 0);
     signal rdata : std_logic_vector(W-1 downto 0);
     signal wdata : std_logic_vector(W-1 downto 0);
     signal we : std_logic := '0';
@@ -49,7 +49,7 @@ begin
         wait until rising_edge(clk);
 
         for i in 0 to 2**N-1 loop
-            address <= std_logic_vector(to_unsigned(i, W));
+            address <= std_logic_vector(to_unsigned(i, N));
             wdata <= std_logic_vector(to_unsigned(i, W));
             we <= '1';
             wait until rising_edge(clk);
@@ -57,7 +57,7 @@ begin
 
         for i in 0 to 2**N-1 loop
             we <= '0';
-            address <= std_logic_vector(to_unsigned(i, W));
+            address <= std_logic_vector(to_unsigned(i, N));
             wait until rising_edge(clk);
         end loop;
 
