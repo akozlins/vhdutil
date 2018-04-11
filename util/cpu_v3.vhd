fbc5f0f32fa976e6acf887ea731e1997b9a69491
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-04-11 18:34:10 +0200

    [cpu] optimize v3

--- util/cpu_v3.vhd
+++ util/cpu_v3.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
 
 use work.util.all;
 
@@ -20,34 +19,34 @@ architecture arch of cpu_v3 is
 
     type state_t is (
         S_FETCH,
-        S_REG_READ,
+        S_REG,
         S_ALU,
-        S_REG_WRITE,
+        S_STORE,
         S_LOAD,
         S_LOADI,
-        S_STORE,
+        S_JUMP,
         S_RESET--,
     );
-    signal state : state_t;
+    signal state, next_state : state_t;
 
     signal ram_addr : word_t;
     signal ram_rd : word_t;
     signal ram_wd : word_t;
     signal ram_we : std_logic;
 
-    signal regA, regB : word_t;
-    signal regA_addr, regB_addr, regC_addr : std_logic_vector(3 downto 0);
+    signal regA_rd, regB_rd, regA_rd_q, regB_rd_q : word_t;
+    signal regA_addr, regB_addr : std_logic_vector(3 downto 0);
     signal regB_wd : word_t;
     signal regB_we : std_logic;
 
     signal alu_op : std_logic_vector(2 downto 0);
-    signal alu_a, alu_b, alu_y : word_t;
+    signal alu_a, alu_b, alu_y, alu_y_q : word_t;
     signal alu_ci, alu_z, alu_s, alu_v, alu_co : std_logic;
 
     -- program counter
-    signal pc : word_t;
+    signal pc, next_pc : word_t;
     -- instruction register
-    signal ir : std_logic_vector(3 downto 0);
+    signal ir : word_t;
     -- flags register (carry, overflow, sign, zero)
     signal flags : std_logic_vector(3 downto 0);
 
@@ -75,8 +74,8 @@ begin
     port map (
         a_addr  => regA_addr,
         b_addr  => regB_addr,
-        a_rd    => regA,
-        b_rd    => regB,
+        a_rd    => regA_rd,
+        b_rd    => regB_rd,
         b_wd    => regB_wd,
         b_we    => regB_we,
         clk     => clk--,
@@ -98,93 +97,105 @@ begin
         co  => alu_co--,
     );
 
+    --
+    --              FETCH       JUMP        REG         ALU         STORE       LOAD        LOADI
+    --  ram_addr    pc          -           -           -           alu_y_q     alu_y_q     alu_y_q
+    --  ram_wd      -           -           -           -           regB_rd     -           -
+    --  ram_we      0           0           0           0           1           0           0
+    --  regA_addr   -           -           ir(3..0)    -           -           -           -
+    --  regB_addr   -           -           ir(7..4)    ir(11..8)   ir(11..8)   ir(11..8)   ir(11..8)
+    --  regB_wd     -           -           -           alu_y       -           ram_rd      ram_rd
+    --  regB_we     0           0           0           1           0           1           1
+    --  alu_a       pc          pc          -           regA_rd_q   -           -           pc
+    --  alu_b       1           ir(7..0)    -           regB_rd_q   -           -           2
+    --
+
+    ram_addr <= pc when ( state = S_FETCH ) else
+                alu_y_q when ( state = S_STORE or state = S_LOAD or state = S_LOADI) else
+                (others => '-');
+    ram_wd <= regB_rd when ( state = S_STORE ) else
+              (others => '-');
+    ram_we <= '1' when ( state = S_STORE ) else
+              '0';
+
+    regA_addr <= ir(3 downto 0) when ( state = S_REG ) else
+                 (others => '-');
+    regB_addr <= ir(7 downto 4) when ( state = S_REG ) else
+                 ir(11 downto 8) when ( state = S_ALU and ir(ir'left) = '0' ) else
+                 ir(11 downto 8) when ( state = S_STORE or state = S_LOAD or state = S_LOADI ) else
+                 (others => '-');
+    regB_wd <= alu_y when ( state = S_ALU and ir(ir'left) = '0' ) else
+               ram_rd when ( state = S_LOAD or state = S_LOADI ) else
+               (others => '-');
+    regB_we <= '1' when ( state = S_ALU and ir(ir'left) = '0' ) else
+               '1' when ( state = S_LOAD or state = S_LOADI ) else
+               '0';
+
+    alu_ci <= flags(3) when ( state = S_ALU and ir(ir'left) = '0' ) else
+              '-';
+    alu_a <= regA_rd_q when ( state = S_ALU ) else
+             pc when ( state = S_FETCH or state = S_JUMP or state = S_LOADI ) else
+             (others => '-');
+    alu_b <= regB_rd_q when ( state = S_ALU ) else
+             X"0001" when ( state = S_FETCH ) else
+             (7 downto 0 => ir(7)) & ir(7 downto 0) when ( state = S_JUMP ) else
+             X"0002" when ( state = S_LOADI ) else
+             (others => '-');
+    alu_op <= ir(14 downto 12) when ( state = S_ALU and ir(ir'left) = '0' ) else
+              (others => '0') when ( state = S_FETCH or state = S_JUMP or state = S_ALU or state = S_LOADI ) else
+              (others => '-');
+
+    next_state <=
+        -- STORE : *(regB + regA) = regC
+        S_STORE when ( state = S_ALU and ir(15 downto 12) = X"F" ) else
+        -- LOAD : regC = *(regB + regA)
+        S_LOAD  when ( state = S_ALU and ir(15 downto 12) = X"E" ) else
+        -- LOADI : regC = *(pc + 1)
+        S_LOADI when ( state = S_FETCH and ram_rd(15 downto 12) = X"D" ) else
+        -- DEBUG
+        S_FETCH when ( state = S_REG and ir(15 downto 12) = X"C" ) else
+        -- JUMP : pc += ram_rd(7 downto 0)
+        S_JUMP  when ( state = S_FETCH and ram_rd(15 downto 12) = X"A" ) else
+        --
+        S_REG   when ( state = S_FETCH ) else
+        S_ALU   when ( state = S_REG ) else
+        S_FETCH;
+
     process(clk, areset)
     begin
     if areset = '1' then
         state <= S_FETCH;
-        ram_addr <= (others => '0');
         pc <= (others => '0');
 
     elsif rising_edge(clk) then
-        ram_we <= '0';
-        regB_we <= '0';
 
-        case state is
-        when S_FETCH =>
-            state <= S_REG_READ;
-            ir <= ram_rd(15 downto 12);
-            regC_addr <= ram_rd(11 downto 8);
-            regB_addr <= ram_rd(7 downto 4);
-            regA_addr <= ram_rd(3 downto 0);
+        alu_y_q <= alu_y;
 
-        when S_REG_READ =>
-            state <= S_ALU;
-            alu_ci <= flags(3);
-            alu_a <= regA;
-            alu_b <= regB;
-            alu_op <= "000";
+        if ( state = S_REG ) then
+            regA_rd_q <= regA_rd;
+            regB_rd_q <= regB_rd;
+        end if;
 
-            case ir is
-            when X"F" => -- ST : *(regB + regA) = regC
-                regB_addr <= regC_addr;
-            when X"E" => -- LD : regC = *(regB + regA)
-                null;
-            when X"D" => -- LDI : regC = *(pc + 1)
-                state <= S_LOADI;
-                ram_addr <= pc + 1;
-                pc <= pc + 1;
-            when X"C" => -- DBG
-                dbg_out <= regB & regA;
-                state <= S_FETCH;
-                ram_addr <= pc + 1;
-                pc <= pc + 1;
-            when X"A" => -- JMP : pc += ram_rd(7 downto 0)
-                alu_a <= pc;
-                alu_b <= ((7 downto 0 => regB_addr(regB_addr'left)) & regB_addr & regA_addr);
-            when "0---" => -- ALU
-                alu_op <= ir(2 downto 0);
-            when others =>
-                null;
-            end case;
+        if ( state = S_FETCH ) then
+            ir <= ram_rd;
+            next_pc <= alu_y; -- next_pc <= pc + 1
+        end if;
 
-        when S_ALU =>
-            case ir is
-            when X"F" => -- ST : *(regB + regA) = regC
-                state <= S_STORE;
-                ram_addr <= alu_y;
-                ram_wd <= regB;
-                ram_we <= '1';
-            when X"E" => -- LD : regC = *(regB + regA)
-                state <= S_LOAD;
-                ram_addr <= alu_y;
-            when X"A" => -- JMP : pc += ram_rd(7 downto 0)
-                state <= S_FETCH;
-                ram_addr <= alu_y;
-                pc <= alu_y;
-            when "0---" => -- ALU
-                state <= S_REG_WRITE;
-                regB_addr <= regC_addr;
-                regB_wd <= alu_y;
-                regB_we <= '1';
-                flags <= alu_co & alu_v & alu_s & alu_z;
-            when others =>
-                null;
-            end case;
+        if ( state = S_REG and ir(15 downto 12) = X"C") then -- DEBUG
+            dbg_out <= regB_rd & regA_rd;
+        end if;
 
-        when S_REG_WRITE | S_STORE =>
-            state <= S_FETCH;
-            ram_addr <= pc + 1;
-            pc <= pc + 1;
+        if ( state = S_ALU and ir(ir'left) = '0' ) then
+            flags <= alu_co & alu_v & alu_s & alu_z;
+        end if;
 
-        when S_LOAD | S_LOADI =>
-            state <= S_REG_WRITE;
-            regB_addr <= regC_addr;
-            regB_wd <= ram_rd;
-            regB_we <= '1';
+        if ( state = S_JUMP or state = S_LOADI ) then
+            pc <= alu_y;
+        elsif ( next_state = S_FETCH ) then
+            pc <= next_pc;
+        end if;
 
-        when others =>
-            null;
-        end case;
+        state <= next_state;
 
     end if; -- rising_edge
     end process;
