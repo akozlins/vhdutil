3d5f8c5d8858e30b29893c38ef580f85caf1ddc4
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-05-02 07:19:26 +0200

    [adder] generate carry4 units in one loop

--- util/10-adder.vhd
+++ util/10-adder.vhd
@@ -16,7 +16,7 @@ entity adder is
     );
 end entity;
 
-architecture arch of adder is
+architecture arch1 of adder is
 
     signal s_i : std_logic_vector(W downto 0);
 
@@ -24,7 +24,19 @@ begin
 
     s_i <= ('0' & a) + b + ci;
     s <= s_i(s'range);
-    co <= s_i(W);
+    co <= s_i(s_i'left);
+
+end architecture;
+
+architecture arch2 of adder is
+
+    signal s_i : std_logic_vector(W+1 downto 0);
+
+begin
+
+    s_i <= ('0' & a & '1') + ('0' & b & ci);
+    s <= s_i(W downto 1);
+    co <= s_i(s_i'left);
 
 end architecture;
 
@@ -38,6 +50,9 @@ architecture rtl of adder is
     signal a_i, b_i, ab_i, s_i : std_logic_vector(4*N-1 downto 0);
     signal co_i : std_logic_vector(4*N-1 downto 0);
 
+    signal ci_i : std_logic_vector(4*N-1 downto 0);
+    signal cyinit_i : std_logic_vector(N-1 downto 0);
+
 begin
 
     a_i(a'range) <= a;
@@ -46,24 +61,17 @@ begin
     s <= s_i(s'range);
     co <= co_i(W-1);
 
-    i_carry4_0 : CARRY4
-    port map (
-        CO => co_i(3 downto 0),
-        O => s_i(3 downto 0),
-        CI => '0',
-        CYINIT => ci,
-        DI => a_i(3 downto 0),
-        S => ab_i(3 downto 0)
-    );
+    ci_i <= std_logic_vector(unsigned(co_i) sll 4);
+    cyinit_i <= (0 => ci, others => '0');
 
     gen:
-    for i in 1 to N-1 generate
+    for i in 0 to N-1 generate
     i_carry4_i : CARRY4
     port map (
         CO => co_i(4*i+3 downto 4*i),
         O => s_i(4*i+3 downto 4*i),
-        CI => co_i(4*i-1),
-        CYINIT => '0',
+        CI => ci_i(4*i+3),
+        CYINIT => cyinit_i(i),
         DI => a_i(4*i+3 downto 4*i),
         S => ab_i(4*i+3 downto 4*i)
     );
