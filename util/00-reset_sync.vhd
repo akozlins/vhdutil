bf1b40bc2202d053341551dba784faa6d0d7db3c
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-06-11 18:10:09 +0200

    add/use sync_chain

--- util/00-reset_sync.vhd
+++ util/00-reset_sync.vhd
@@ -16,20 +16,15 @@ end entity;
 
 architecture arch of reset_sync is
 
-    signal rst_n_q : std_logic_vector(N downto 0);
-
 begin
 
-    rstout_n <= rst_n_q(N);
-
-    process(clk, rst_n)
-    begin
-    if ( rst_n = '0' ) then
-        rst_n_q <= (others => '0');
-    elsif rising_edge(clk) then
-        rst_n_q <= rst_n_q(N-1 downto 0) & '1';
-    end if;
-    end process;
-
+    i_sync_chain : entity work.sync_chain
+    generic map ( N => N, W => 1 )
+    port map (
+        data_out(0) => rstout_n,
+        data_in(0) => '1',
+        rst_n => rst_n,
+        clk => clk--,
+    );
 
 end architecture;
