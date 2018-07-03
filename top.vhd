c0abefd902e71d41d10471fa171be85c428b6e3a
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-07-03 11:38:56 +0200

    [ip_clk] inst PLLE2_BASE
     - [top] gen clk_200

--- top.vhd
+++ top.vhd
@@ -13,12 +13,10 @@ end entity;
 architecture arch of top is
 
     signal clk_100, rst_100_n : std_logic;
-
-    constant CLK_MHZ : positive := 50;
-
-    signal clk, rst_n : std_logic;
+    signal clk_200, rst_200_n, clk_200_locked : std_logic;
 
     signal btn_i : std_logic_vector(pl_btn'range);
+
     signal dbg_i : std_logic_vector(15 downto 0);
 
 begin
@@ -28,59 +26,46 @@ begin
     i_rst_100_n : entity work.reset_sync
     port map ( rstout_n => rst_100_n, arst_n => not pl_btn(0), clk => clk_100 );
 
-    i_clk : entity work.clkdiv
-    generic map (
-        P => 100 / CLK_MHZ--,
-    )
+    i_clk_200 : entity work.ip_clk
+    generic map ( M => 8, D => 4 )
     port map (
-        clkout => clk,
+        clkout => clk_200,
+        locked => clk_200_locked,
         rst_n => rst_100_n,
         clk => clk_100--,
     );
 
-    i_clk_ok : entity work.clkmon
+    i_rst_200_n : entity work.reset_sync
+    port map ( rstout_n => rst_200_n, arst_n => rst_100_n and clk_200_locked, clk => clk_200 );
+
+    i_clk_200_ok : entity work.clkmon
     generic map (
-        TST_MHZ => CLK_MHZ,
+        TST_MHZ => 200,
         CLK_MHZ => 100--,
     )
     port map (
-        tst_clk => clk,
+        tst_clk => clk_200,
         tst_ok => pl_led(6),
         rst_n => rst_100_n,
         clk => clk_100--,
     );
 
-    i_rst : entity work.reset_sync
-    port map ( rstout_n => rst_n, arst_n => rst_100_n, clk => clk );
-
-    i_clk_hz : entity work.clkdiv
+    i_clk_200_hz : entity work.clkdiv
     generic map (
-        P => CLK_MHZ * 10**6--;
+        P => 200 * 10**6--;
     )
     port map (
         clkout => pl_led(7),
-        rst_n => rst_n,
-        clk => clk--,
-    );
-
-    i_btn : entity work.debouncer
-    generic map (
-        W => pl_btn'length,
-        N => 16#FFFF#--,
-    )
-    port map (
-        d => pl_btn,
-        q => btn_i,
-        rst_n => rst_n,
-        clk => clk--,
+        rst_n => rst_200_n,
+        clk => clk_200--,
     );
 
     i_cpu : entity work.cpu16_v4
     port map (
         dbg_out => dbg_i,
         dbg_in => (others => '-'),
-        clk => clk_100,
-        rst_n => rst_100_n--,
+        clk => clk_200,
+        rst_n => rst_200_n--,
     );
 
     pl_led(3 downto 0) <= dbg_i(11 downto 8);
---
+++ util/ip_clk.vhd
@@ -0,0 +1,91 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ip_clk is
+    generic (
+        M : positive := 8;
+        D : positive := 4--;
+    );
+    port (
+        clkout  :   out std_logic;
+        locked  :   out std_logic;
+        rst_n   :   in  std_logic;
+        clk     :   in  std_logic--;
+    );
+end entity;
+
+library unisim;
+use unisim.vcomponents.all;
+
+architecture arch of ip_clk is
+
+    signal pwrdwn : std_logic;
+    signal clkfbin, clkfbout : std_logic;
+    signal clkout0, clkout1, clkout2, clkout3, clkout4, clkout5 : std_logic;
+    signal clkin1 : std_logic;
+    signal rst : std_logic;
+
+begin
+
+    pwrdwn <= '0';
+    clkfbin <= clkfbout;
+
+    clkout <= clkout0;
+    clkin1 <= clk;
+    rst <= not rst_n;
+
+   -- PLLE2_BASE: Base Phase Locked Loop (PLL)
+   --             Artix-7
+   -- Xilinx HDL Language Template, version 2018.1
+
+   PLLE2_BASE_inst : PLLE2_BASE
+   generic map (
+      BANDWIDTH => "OPTIMIZED",  -- OPTIMIZED, HIGH, LOW
+      CLKFBOUT_MULT => M,        -- Multiply value for all CLKOUT, (2-64)
+      CLKFBOUT_PHASE => 0.0,     -- Phase offset in degrees of CLKFB, (-360.000-360.000).
+      CLKIN1_PERIOD => 0.0,      -- Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
+      -- CLKOUT0_DIVIDE - CLKOUT5_DIVIDE: Divide amount for each CLKOUT (1-128)
+      CLKOUT0_DIVIDE => D,
+      CLKOUT1_DIVIDE => 1,
+      CLKOUT2_DIVIDE => 1,
+      CLKOUT3_DIVIDE => 1,
+      CLKOUT4_DIVIDE => 1,
+      CLKOUT5_DIVIDE => 1,
+      -- CLKOUT0_DUTY_CYCLE - CLKOUT5_DUTY_CYCLE: Duty cycle for each CLKOUT (0.001-0.999).
+      CLKOUT0_DUTY_CYCLE => 0.5,
+      CLKOUT1_DUTY_CYCLE => 0.5,
+      CLKOUT2_DUTY_CYCLE => 0.5,
+      CLKOUT3_DUTY_CYCLE => 0.5,
+      CLKOUT4_DUTY_CYCLE => 0.5,
+      CLKOUT5_DUTY_CYCLE => 0.5,
+      -- CLKOUT0_PHASE - CLKOUT5_PHASE: Phase offset for each CLKOUT (-360.000-360.000).
+      CLKOUT0_PHASE => 0.0,
+      CLKOUT1_PHASE => 0.0,
+      CLKOUT2_PHASE => 0.0,
+      CLKOUT3_PHASE => 0.0,
+      CLKOUT4_PHASE => 0.0,
+      CLKOUT5_PHASE => 0.0,
+      DIVCLK_DIVIDE => 1,        -- Master division value, (1-56)
+      REF_JITTER1 => 0.0,        -- Reference input jitter in UI, (0.000-0.999).
+      STARTUP_WAIT => "FALSE"    -- Delay DONE until PLL Locks, ("TRUE"/"FALSE")
+   )
+   port map (
+      -- Clock Outputs: 1-bit (each) output: User configurable clock outputs
+      CLKOUT0 => CLKOUT0,   -- 1-bit output: CLKOUT0
+      CLKOUT1 => CLKOUT1,   -- 1-bit output: CLKOUT1
+      CLKOUT2 => CLKOUT2,   -- 1-bit output: CLKOUT2
+      CLKOUT3 => CLKOUT3,   -- 1-bit output: CLKOUT3
+      CLKOUT4 => CLKOUT4,   -- 1-bit output: CLKOUT4
+      CLKOUT5 => CLKOUT5,   -- 1-bit output: CLKOUT5
+      -- Feedback Clocks: 1-bit (each) output: Clock feedback ports
+      CLKFBOUT => CLKFBOUT, -- 1-bit output: Feedback clock
+      LOCKED => LOCKED,     -- 1-bit output: LOCK
+      CLKIN1 => CLKIN1,     -- 1-bit input: Input clock
+      -- Control Ports: 1-bit (each) input: PLL control ports
+      PWRDWN => PWRDWN,     -- 1-bit input: Power-down
+      RST => RST,           -- 1-bit input: Reset
+      -- Feedback Clocks: 1-bit (each) input: Clock feedback ports
+      CLKFBIN => CLKFBIN    -- 1-bit input: Feedback clock
+   );
+
+end architecture;
