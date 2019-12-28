d91581f2af5f0e14a8889faaf16b33d726362179
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-12-28 11:22:55 +0100

    [altera] use top.qip, mv a10 scripts

--- altera/DE5a_Net/device.tcl
+++ altera/DE5a_Net/device.tcl
@@ -1,3 +1,5 @@
+#
+
 set_project_property DEVICE_FAMILY {Arria 10}
 set_project_property DEVICE {10AX115N2F45E1SG}
 
--- altera/DE5a_Net/software/hal_bsp.tcl
+++ altera/DE5a_Net/software/hal_bsp.tcl
@@ -1,1 +1,3 @@
-include/a10/hal_bsp.tcl
+#
+
+source [ file join [ file dirname $argv0 ] "include/a10/hal_bsp.tcl" ]
