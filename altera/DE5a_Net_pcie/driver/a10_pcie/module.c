e2c47f88ed06b076142213e5cc588ee298fb1d97
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-12-29 10:30:54 +0100

    [altera] update a10_pcie module structure

---
+++ altera/DE5a_Net_pcie/driver/a10_pcie/module.c
@@ -0,0 +1,52 @@
+
+#include <linux/module.h>
+#include <linux/pci.h>
+
+MODULE_AUTHOR("Alexandr Kozlinskiy");
+MODULE_LICENSE("GPL");
+
+#define DEVICE_NAME "a10_pcie"
+#define VENDOR_ID 0x1172
+#define DEVICE_ID 0xE001
+
+static
+struct pci_device_id id_table[] = {
+    { PCI_DEVICE(VENDOR_ID, DEVICE_ID), },
+    { 0, },
+};
+MODULE_DEVICE_TABLE(pci, id_table);
+
+#include "probe.h"
+
+static
+struct pci_driver pci_driver = {
+    .name = DEVICE_NAME,
+    .id_table = id_table,
+    .probe = probe,
+    .remove = remove,
+};
+
+static
+int __init _module_init(void) {
+    int err;
+
+    pr_info("[%s] init\n", DEVICE_NAME);
+
+    err = pci_register_driver(&pci_driver);
+    if(err < 0) {
+        pr_warn("[%s] pci_register_driver() failed\n", DEVICE_NAME);
+        return err;
+    }
+
+    return 0;
+}
+
+static
+void __exit _module_exit(void) {
+    pr_info("[%s] exit\n", DEVICE_NAME);
+
+    pci_unregister_driver(&pci_driver);
+}
+
+module_init(_module_init);
+module_exit(_module_exit);
---
+++ altera/DE5a_Net_pcie/driver/a10_pcie/probe.h
@@ -0,0 +1,45 @@
+
+static struct pci_dev *pci_dev = NULL;
+
+struct bar_t {
+    void __iomem *base;
+};
+static struct bar_t bars[6];
+
+static
+int probe(struct pci_dev *dev, const struct pci_device_id *id) {
+    int err = 0;
+
+    pr_info("[%s] probe\n", DEVICE_NAME);
+
+    pci_dev = dev;
+
+    err = pci_enable_device(pci_dev);
+    if(err < 0) {
+        dev_err(&(pci_dev->dev), "pci_enable_device\n");
+        goto fail;
+    }
+
+    err = pci_request_region(pci_dev, 0, "bar0");
+    if(err) {
+        dev_err(&(pci_dev->dev), "pci_request_region\n");
+        goto fail;
+    }
+    bars[0].base = pci_iomap(pci_dev, 0, pci_resource_len(pci_dev, 0));
+    pr_info("[%s] bars[%d].base = %p\n", DEVICE_NAME, 0, bars[0].base);
+
+    return 0;
+
+fail:
+    return err;
+}
+
+static
+void remove(struct pci_dev *dev) {
+    pr_info("[%s] remove\n", DEVICE_NAME);
+
+    if(bars[0].base) pci_iounmap(pci_dev, bars[0].base);
+    pci_release_region(pci_dev, 0);
+
+    pci_disable_device(pci_dev);
+}
