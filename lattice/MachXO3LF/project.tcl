b343d1009952105f7502ce053df0f56c3b4b86b3
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-04-10 17:04:25 +0200

    [lattice] update example project

--- lattice/MachXO3LF/project.tcl
+++ lattice/MachXO3LF/project.tcl
@@ -5,24 +5,24 @@ cd "$(dirname -- "$(readlink -e -- "$0")")" || exit 1 ; \
 exec lattice_pnmainc "$0"
 
 set basedir [ file normalize [ file dirname [ info script ] ] ]
-puts $basedir
-file mkdir .cache
-cd .cache
+file mkdir ".cache"
+cd ".cache"
 
-prj_project new -name top -synthesis lse
-prj_dev set -part LCMXO3LF-6900E-5MG256C
+prj_project new -name "top" -lpf "$basedir/top.lpf" -synthesis "synplify"
+prj_dev set -part "LCMXO3LF-6900C-5BG256C"
+
+prj_src add "$basedir/top.vhd"
+prj_impl option top "top"
 
 foreach f [ lsort [ glob -nocomplain -directory $basedir \
-    "top.vhd" \
-    "top.lpf" \
     "../../util/*.vhd" \
     "../../rv/rv32i_*.vhd" \
 ] ] {
-    prj_src add $f
+    prj_src add "$f"
 }
 
 prj_project save
 
-prj_run Synthesis -task Lattice_Synthesis
-prj_run PAR -task PARTrace
+#prj_run Synthesis -task Lattice_Synthesis
+#prj_run PAR -task PARTrace
 #prj_run Export -impl impl1 -task Bitgen
--- lattice/MachXO3LF/top.vhd
+++ lattice/MachXO3LF/top.vhd
@@ -3,44 +3,76 @@ use ieee.std_logic_1164.all;
 
 entity top is
 port (
-    o_led       : out   std_logic_vector(7 downto 0);
-    i_sw        : in    std_logic_vector(3 downto 0);
+    -- 8 light diods (active low)
+    o_led_n     : out   std_logic_vector(7 downto 0);
+    -- 4 switches
+    i_sw_n      : in    std_logic_vector(3 downto 0);
 
+    -- reset button (active low)
     i_reset_n   : in    std_logic;
+    -- external clock
     i_clk       : in    std_logic--;
 );
 end entity;
 
 architecture arch of top is
 
-    signal reset_n : std_logic;
+    signal led : std_logic_vector(o_led_n'range) := (others => '0');
+
+    signal clk, reset_n : std_logic;
 
     signal dbg : std_logic_vector(31 downto 0);
 
+    component osch
+    generic (
+        NOM_FREQ : string := "12.09"--;
+    );
+    port (
+        stdby : in std_logic := '0';
+        sedstdby : out std_logic;
+        osc : out std_logic--;
+    );
+    end component;
+
+    attribute NOM_FREQ : string;
+    attribute NOM_FREQ of e_osch : label is "12.09";
+
 begin
 
-    e_reset_n : entity work.reset_sync
-    port map ( o_reset_n => reset_n, i_reset_n => i_reset_n, i_clk => i_clk );
+    o_led_n <= not led;
 
-    process(i_clk, reset_n)
+    led(3 downto 0) <= not i_sw_n;
+
+    -- internal 12.09 MHz oscillator
+    e_osch : component osch
+    port map (
+        osc => clk--,
+    );
+
+    -- generate reset for clock 'clk'
+    e_reset_n : entity work.reset_sync
+    port map ( o_reset_n => reset_n, i_reset_n => i_reset_n, i_clk => clk );
+
+    e_clk_hz : entity work.clkdiv
+    generic map ( p => 12000000 )
+    port map ( o_clk => led(7), i_reset_n => reset_n, i_clk => clk );
+
+    process(clk, reset_n)
     begin
-    if ( reset_n = '1' ) then
---        o_led <= (others => '0');
+    if ( reset_n = '0' ) then
         --
-    elsif rising_edge(i_clk) then
---        o_led <= (others => '0');
---        o_led(3 downto 0) <= i_sw;
+    elsif rising_edge(clk) then
         --
     end if;
     end process;
 
-    o_led <= dbg(7 downto 0);
+
 
     e_cpu : entity work.rv32i_cpu_v1
     port map (
         dbg_out => dbg,
-        dbg_in => (others => '-'),
-        clk => i_clk,
+        dbg_in => (others => '0'),
+        clk => clk,
         rst_n => reset_n--,
     );
 
