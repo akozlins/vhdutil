be75777b4e95254dea8671a71d31d7020b51b1e3
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-08-12 10:34:10 +0200

    [dmabuf] create platform_device

--- dmabuf/module.c
+++ dmabuf/module.c
@@ -11,13 +11,29 @@ MODULE_LICENSE("GPL");
 
 
 
+#include "dmabuf_platform_device.h"
+
+static struct platform_device* dmabuf_platform_device = NULL;
+
+
+
 static
 int __init dmabuf_module_init(void) {
+    int error = 0;
+
+    dmabuf_platform_device = dmabuf_platform_device_create(DEVICE_NAME);
+    if(IS_ERR_OR_NULL(dmabuf_platform_device)) {
+        error = PTR_ERR(dmabuf_platform_device);
+        dmabuf_platform_device = NULL;
+        return error;
+    }
+
     return 0;
 }
 
 static
 void __exit dmabuf_module_exit(void) {
+    dmabuf_platform_device_delete(dmabuf_platform_device);
 }
 
 module_init(dmabuf_module_init);
