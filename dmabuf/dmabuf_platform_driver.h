e3e25003f9eee2a75033e90cbb33edadc19a0233
akozlins <>
2020-08-13 14:41:53 +0200

    [dmabuf] alloc chrdev

--- dmabuf/dmabuf_platform_driver.h
+++ dmabuf/dmabuf_platform_driver.h
@@ -1,4 +1,5 @@
 
+#include <linux/cdev.h>
 #include <linux/dma-mapping.h>
 #include <linux/slab.h>
 
@@ -53,6 +54,102 @@ err_free:
 
 
 
+struct dmabuf_chrdev {
+    dev_t dev;
+    struct class* class;
+    struct cdev cdev;
+    struct device* device;
+};
+static struct dmabuf_chrdev dmabuf_chrdev;
+
+static
+int dmabuf_chrdev_open(struct inode *inode, struct file *file) {
+    pr_info("[%s/%s]\n", THIS_MODULE->name, __FUNCTION__);
+
+    return 0;
+}
+
+static
+int dmabuf_chrdev_mmap(struct file *filp, struct vm_area_struct *vma) {
+    return -ENOMEM;
+}
+
+static
+struct file_operations dmabuf_chrdev_fops = {
+    .owner = THIS_MODULE,
+    .open = dmabuf_chrdev_open,
+    .mmap = dmabuf_chrdev_mmap,
+};
+
+static
+void dmabuf_chrdev_free(void) {
+    pr_info("[%s/%s]\n", THIS_MODULE->name, __FUNCTION__);
+
+    if(&dmabuf_chrdev.cdev.dev != 0) {
+        cdev_del(&dmabuf_chrdev.cdev);
+        dmabuf_chrdev.cdev.dev = 0;
+    }
+
+    if(dmabuf_chrdev.device != NULL) {
+        device_destroy(dmabuf_chrdev.class, dmabuf_chrdev.dev);
+        dmabuf_chrdev.device = NULL;
+    }
+
+    if(dmabuf_chrdev.class != NULL) {
+        class_destroy(dmabuf_chrdev.class);
+        dmabuf_chrdev.class = NULL;
+    }
+    if(dmabuf_chrdev.dev != 0) {
+        unregister_chrdev_region(dmabuf_chrdev.dev, 1);
+        dmabuf_chrdev.dev = 0;
+    }
+}
+
+static
+int dmabuf_chrdev_alloc(void) {
+    int error = 0;
+
+    pr_info("[%s/%s]\n", THIS_MODULE->name, __FUNCTION__);
+
+    error = alloc_chrdev_region(&dmabuf_chrdev.dev, 0, 1, THIS_MODULE->name);
+    if(error) {
+        dmabuf_chrdev.dev = 0;
+        pr_err("[%s/%s] alloc_chrdev_region: error = %d\n", THIS_MODULE->name, __FUNCTION__, error);
+        goto err_free;
+    }
+
+    dmabuf_chrdev.class = class_create(THIS_MODULE, THIS_MODULE->name);
+    if(IS_ERR_OR_NULL(dmabuf_chrdev.class)) {
+        error = PTR_ERR(dmabuf_chrdev.class);
+        dmabuf_chrdev.class = NULL;
+        pr_err("[%s/%s] class_create: error = %d\n", THIS_MODULE->name, __FUNCTION__, error);
+        goto err_free;
+    }
+
+    cdev_init(&dmabuf_chrdev.cdev, &dmabuf_chrdev_fops);
+    error = cdev_add(&dmabuf_chrdev.cdev, dmabuf_chrdev.dev, 1);
+    if(error) {
+        dmabuf_chrdev.cdev.dev = 0;
+        pr_err("[%s/%s] cdev_add: error = %d\n", THIS_MODULE->name, __FUNCTION__, error);
+        goto err_free;
+    }
+    dmabuf_chrdev.device = device_create(dmabuf_chrdev.class, NULL, dmabuf_chrdev.dev, NULL, THIS_MODULE->name, 0);
+    if(IS_ERR_OR_NULL(dmabuf_chrdev.device)) {
+        error = PTR_ERR(dmabuf_chrdev.device);
+        dmabuf_chrdev.device = NULL;
+        pr_err("[%s/%s] device_create: error = %d\n", THIS_MODULE->name, __FUNCTION__, error);
+        goto err_free;
+    }
+
+    return 0;
+
+err_free:
+    dmabuf_chrdev_free();
+    return error;
+}
+
+
+
 static
 int dmabuf_platform_driver_probe(struct platform_device *pdev) {
     int error = 0;
@@ -70,6 +167,11 @@ int dmabuf_platform_driver_probe(struct platform_device *pdev) {
         goto err_out;
     }
 
+    error = dmabuf_chrdev_alloc();
+    if(error) {
+        goto err_out;
+    }
+
     return 0;
 
 err_out:
@@ -80,6 +182,7 @@ static
 int dmabuf_platform_driver_remove(struct platform_device *pdev) {
     pr_info("[%s/%s]\n", THIS_MODULE->name, __FUNCTION__);
 
+    dmabuf_chrdev_free();
     dmabuf_free(pdev);
 
     return 0;
