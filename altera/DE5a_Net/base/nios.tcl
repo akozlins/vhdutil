23c2224f0c29932ad19e1a91f65ee02b06d73aae
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-10-14 11:13:43 +0200

    [util] mv altera hw modules to util/altera/modules/

--- altera/DE5a_Net/base/nios.tcl
+++ altera/DE5a_Net/base/nios.tcl
@@ -8,4 +8,4 @@ source [ file join [ file dirname [ info script ] ] "device.tcl" ]
 source [ file join [ file dirname [ info script ] ] "util/nios_base.tcl" ]
 set_instance_parameter_value ram {memorySize} {0x00080000}
 
-source [ file join [ file dirname [ info script ] ] "a10/flash1616.tcl" ]
+source [ file join [ file dirname [ info script ] ] "util/altera/modules/flash1616.tcl" ]
