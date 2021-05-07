a711d24f11059114e34ad44740415dbdedb05d9a
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-05-07 16:05:45 +0200

    [max10] assignments.tcl - add/use set_assignment

--- altera/max10_dev/assignments.tcl
+++ altera/max10_dev/assignments.tcl
@@ -3,29 +3,17 @@
 # Configuration mode: Single Uncompressed Image with Memory Initialization
 set_global_assignment -name INTERNAL_FLASH_UPDATE_MODE "SINGLE IMAGE WITH ERAM"
 
-set_location_assignment PIN_120 -to i_sw[0]
-set_location_assignment PIN_124 -to i_sw[1]
-set_location_assignment PIN_127 -to i_sw[2]
-set_location_assignment PIN_130 -to i_sw[3]
-set_location_assignment PIN_131 -to i_sw[4]
+proc set_assignment { name pins } {
+    set i 0
+    foreach pin $pins {
+        set_location_assignment $pin -to $name\[$i\]
+        set_instance_assignment -name IO_STANDARD "2.5 V" -to $name\[$i\]
+        set i [ expr $i + 1 ]
+    }
+}
 
-set_instance_assignment -name IO_STANDARD "2.5 V" -to i_sw[0]
-set_instance_assignment -name IO_STANDARD "2.5 V" -to i_sw[1]
-set_instance_assignment -name IO_STANDARD "2.5 V" -to i_sw[2]
-set_instance_assignment -name IO_STANDARD "2.5 V" -to i_sw[3]
-set_instance_assignment -name IO_STANDARD "2.5 V" -to i_sw[4]
-
-set_location_assignment PIN_132 -to o_led_n[0]
-set_location_assignment PIN_134 -to o_led_n[1]
-set_location_assignment PIN_135 -to o_led_n[2]
-set_location_assignment PIN_140 -to o_led_n[3]
-set_location_assignment PIN_141 -to o_led_n[4]
-
-set_instance_assignment -name IO_STANDARD "2.5 V" -to o_led_n[0]
-set_instance_assignment -name IO_STANDARD "2.5 V" -to o_led_n[1]
-set_instance_assignment -name IO_STANDARD "2.5 V" -to o_led_n[2]
-set_instance_assignment -name IO_STANDARD "2.5 V" -to o_led_n[3]
-set_instance_assignment -name IO_STANDARD "2.5 V" -to o_led_n[4]
+set_assignment i_sw { PIN_120 PIN_124 PIN_127 PIN_130 PIN_131 }
+set_assignment o_led_n { PIN_132 PIN_134 PIN_135 PIN_140 PIN_141 }
 
 set_location_assignment PIN_121 -to i_reset_n
 set_instance_assignment -name IO_STANDARD "2.5 V" -to i_reset_n
