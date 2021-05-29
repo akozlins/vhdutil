1274bae3bf7f40a093bfef2949df30526dfad2d6
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-05-29 13:09:30 +0200

    [mu3e_feb_v1] test spi_master

--- altera/mu3e_feb_v1/software/main.cpp
+++ altera/mu3e_feb_v1/software/main.cpp
@@ -22,6 +22,26 @@ int main() {
             break;
         case 't': {
             auto base = (volatile alt_u32*)AVM_TEST_BASE;
+            base[0] = 0x0001;
+            printf("ctrl = 0x%08X\n", base[0]);
+
+            base[1] = 0x00; // Set Addr
+            base[1] = 0x02; // Addr
+            base[1] = 0x80; // Read Data
+            base[1] = 0x00; // Data
+
+            alt_u32 r;
+            while(base[0] & 0x00020000) { printf("."); usleep(1); }
+            r = base[1];
+            while(base[0] & 0x00020000) { printf("."); usleep(1); }
+            r = base[1];
+            while(base[0] & 0x00020000) { printf("."); usleep(1); }
+            r = base[1];
+            while(base[0] & 0x00020000) { printf("."); usleep(1); }
+            r = base[1];
+            printf("\n");
+            printf("r = 0x%08X\n", r);
+
             break;
         }
         default:
--- altera/mu3e_feb_v1/top.vhd
+++ altera/mu3e_feb_v1/top.vhd
@@ -33,6 +33,10 @@ architecture arch of top is
 
     signal av_test : work.util.avalon_t;
 
+    signal spi_wdata, spi_rdata : std_logic_vector(31 downto 0);
+    signal spi_we, spi_wfull, spi_rack, spi_rempty : std_logic;
+    signal spi_sclk_div : std_logic_vector(15 downto 0);
+
 begin
 
     o_led_n <= not led;
@@ -57,10 +61,10 @@ begin
 
     e_nios : component work.components.nios
     port map (
-        spi_miso => i_si5342_spi_miso,
-        spi_mosi => o_si5342_spi_mosi,
-        spi_sclk => o_si5342_spi_sclk,
-        spi_ss_n(0) => o_si5342_spi_ss_n,
+--        spi_sclk => o_si5342_spi_sclk,
+--        spi_mosi => o_si5342_spi_mosi,
+--        spi_miso => i_si5342_spi_miso,
+--        spi_ss_n(0) => o_si5342_spi_ss_n,
 
         avm_test_address        => av_test.address(7 downto 0),
         avm_test_read           => av_test.read,
@@ -77,9 +81,56 @@ begin
     begin
     if ( reset_50_n = '0' ) then
         av_test.readdata <= X"CCCCCCCC";
+        spi_we <= '0';
+        spi_rack <= '0';
     elsif rising_edge(clk_50) then
         av_test.readdata <= X"CCCCCCCC";
+        spi_we <= '0';
+        spi_rack <= '0';
+
+        if ( av_test.write = '1' and av_test.address(7 downto 0) = X"00" ) then
+            spi_sclk_div <= av_test.writedata(15 downto 0);
+        end if;
+        if ( av_test.read = '1' and av_test.address(7 downto 0) = X"00" ) then
+            av_test.readdata <= (others => '0');
+            av_test.readdata(15 downto 0) <= spi_sclk_div;
+            av_test.readdata(16) <= spi_wfull;
+            av_test.readdata(17) <= spi_rempty;
+--            av_test.readdata(18) <= spi_cpol;
+--            av_test.readdata(19) <= spi_sdo_cpha;
+--            av_test.readdata(10) <= spi_sdi_cpha;
+        end if;
+
+        if ( av_test.write = '1' and av_test.address(7 downto 0) = X"01" and spi_wfull = '0' ) then
+            spi_wdata <= av_test.writedata;
+            spi_we <= '1';
+        end if;
+        if ( av_test.read = '1' and av_test.address(7 downto 0) = X"01" and spi_rempty = '0' ) then
+            av_test.readdata <= spi_rdata;
+            spi_rack <= '1';
+        end if;
     end if;
     end process;
+
+    e_spi : entity work.spi_master
+    port map (
+        o_sclk => o_si5342_spi_sclk,
+        o_sdo => o_si5342_spi_mosi,
+        i_sdi => i_si5342_spi_miso,
+        o_ss_n => o_si5342_spi_ss_n,
+
+        i_wdata => spi_wdata(7 downto 0),
+        i_we => spi_we,
+        o_wfull => spi_wfull,
+
+        o_rdata => spi_rdata(7 downto 0),
+        i_rack => spi_rack,
+        o_rempty => spi_rempty,
+
+        i_sclk_div => spi_sclk_div,
+
+        i_reset_n => reset_50_n,
+        i_clk => clk_50--,
+    );
 
 end architecture;
