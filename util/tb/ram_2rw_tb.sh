1c02f6e66a0def3fbfbd1ed24d0729ae8fea37cb
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-01-18 18:24:57 +0100

    [util] add ram_2rw_tb

---
+++ util/tb/ram_2rw_tb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+IFS="$(printf '\n\t')"
+unset CDPATH
+cd "$(dirname -- "$(readlink -e -- "$0")")" || exit 1
+
+entity=$(basename "$0" .sh)
+
+STOP_TIME_US=10 \
+../sim.sh "$entity" \
+    ../*.vhd \
+    "$entity.vhd"
