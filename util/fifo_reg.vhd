59ef2dd83ec572025207a14fa8b98a127170bd01
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-01-18 18:26:01 +0100

    [util] fifo_reg - rename data -> cell

--- util/fifo_reg.vhd
+++ util/fifo_reg.vhd
@@ -32,21 +32,21 @@ end entity;
 
 architecture arch of fifo_reg is
 
-    type data_array_t is array (natural range <>) of std_logic_vector(g_DATA_WIDTH-1 downto 0);
-    signal data : data_array_t(0 to g_N+1) := (others => (others => '-'));
+    type cell_array_t is array (natural range <>) of std_logic_vector(g_DATA_WIDTH-1 downto 0);
+    signal cell : cell_array_t(0 to g_N+1) := (others => (others => '-'));
     signal empty : std_logic_vector(0 to g_N+1) := (0 => '0', others => '1');
 
 begin
 
     -- empty(0) is used when i_rack = '0' and index is 1
     empty(0) <= '0';
-    data(0) <= (others => '-');
-    -- data(g_N+1) is used when i_rack = '1' and index is g_N
+    cell(0) <= (others => '-');
+    -- cell(g_N+1) is used when i_rack = '1' and index is g_N
     empty(g_N+1) <= '1';
-    data(g_N+1) <= (others => '-');
+    cell(g_N+1) <= (others => '-');
 
-    -- data(1) contains readable data
-    o_rdata <= data(1);
+    -- cell(1) contains readable data
+    o_rdata <= cell(1);
     o_rempty <= empty(1);
     o_wfull <= not empty(g_N);
 
@@ -54,25 +54,25 @@ begin
     begin
     if ( i_reset_n = '0' ) then
         for i in 1 to g_N loop
-            data(i) <= (others => '-');
+            cell(i) <= (others => '-');
             empty(i) <= '1';
         end loop;
     elsif rising_edge(i_clk) then
         for i in 1 to g_N loop
             if ( i_rack = '1' and empty(1) = '0' ) then
-                -- read from data(1) and shift all data left
-                data(i) <= data(i+1);
+                -- read from cell(1) and shift left
+                cell(i) <= cell(i+1);
                 empty(i) <= empty(i+1);
-                -- read from fifo to first empty data
-                -- NOTE: do not touch last data (o_wfull is '1')
+                -- copy wdata into first empty cell
+                -- NOTE: do not touch last cell (o_wfull is '1')
                 if ( empty(i to i+1) = "01" and i < g_N ) then
-                    data(i) <= i_wdata;
+                    cell(i) <= i_wdata;
                     empty(i) <= not i_we;
                 end if;
             else
-                -- read from fifo to first empty data
+                -- copy wdata into first empty cell
                 if ( empty(i-1 to i) = "01" ) then
-                    data(i) <= i_wdata;
+                    cell(i) <= i_wdata;
                     empty(i) <= not i_we;
                 end if;
             end if;
