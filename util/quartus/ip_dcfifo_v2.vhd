2a6bcb0586afddcd8f3a798fb733329bd3dda7ce
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-01-18 18:38:58 +0100

    [util] refactor ip_dcfifo_v2
    - add g_SHOWAHEAD, g_R/WREG_N
    - use fifo_reg to register read and write ports

--- util/quartus/ip_dcfifo_v2.vhd
+++ util/quartus/ip_dcfifo_v2.vhd
@@ -15,20 +15,23 @@ entity ip_dcfifo_v2 is
 generic (
     g_ADDR_WIDTH : positive := 8;
     g_DATA_WIDTH : positive := 8;
-    g_DEVICE_FAMILY : string := ""--;
+    g_SHOWAHEAD : string := "ON";
+    g_RREG_N : positive := 2;
+    g_WREG_N : positive := 2;
+    g_DEVICE_FAMILY : string := "Arria 10"--;
 );
 port (
+    o_rdata     : out   std_logic_vector(g_DATA_WIDTH-1 downto 0);
+    i_rack      : in    std_logic; -- read enable (request, acknowledge)
+    o_rempty    : out   std_logic;
+    o_rusedw    : out   std_logic_vector(g_ADDR_WIDTH-1 downto 0);
+    i_rclk      : in    std_logic;
+
     i_wdata     : in    std_logic_vector(g_DATA_WIDTH-1 downto 0);
     i_we        : in    std_logic; -- write enable (request)
     o_wfull     : out   std_logic;
     o_wusedw    : out   std_logic_vector(g_ADDR_WIDTH-1 downto 0);
     i_wclk      : in    std_logic;
-
-    o_rdata     : out   std_logic_vector(g_DATA_WIDTH-1 downto 0);
-    i_re        : in    std_logic; -- read enable (request, acknowledge)
-    o_rempty    : out   std_logic;
-    o_rusedw    : out   std_logic_vector(g_ADDR_WIDTH-1 downto 0);
-    i_rclk      : in    std_logic;
 
     -- async clear
     i_reset_n   : in    std_logic--;
@@ -40,11 +43,8 @@ use altera_mf.altera_mf_components.all;
 
 architecture arch of ip_dcfifo_v2 is
 
-    signal reset_n : std_logic;
-
-    signal fifo_rdata : std_logic_vector(o_rdata'range);
-    signal fifo_re : std_logic;
-    signal fifo_rempty : std_logic;
+    signal fifo_rdata, fifo_wdata : std_logic_vector(g_DATA_WIDTH-1 DOWNTO 0);
+    signal fifo_rack, fifo_rempty, rreset_n, fifo_we, fifo_wfull, wreset_n : std_logic;
 
 begin
 
@@ -63,10 +63,10 @@ begin
         -- Specifies the width of the data and q ports for the SCFIFO function and DCFIFO function. 
         lpm_width => g_DATA_WIDTH,
         -- Specifies whether the FIFO is in normal mode (OFF) or show-ahead mode (ON).
-        lpm_showahead => "ON",
+        lpm_showahead => g_SHOWAHEAD,
         -- Specifies whether to register the q output.
 --        add_ram_output_register => "OFF",
-        -- Specifies whether or not the FIFO Intel FPGA IP core is constructed using the RAM blocks.
+        -- Specifies whether or not the FIFO Intel FPGA IP core is constructed using the RAM Embedded Array Blocks.
         use_eab => "ON",
         -- Specifies whether or not to enable the protection circuitry
         -- for overflow/underflow checking that disables the wrreq/rdreq port
@@ -86,41 +86,75 @@ begin
         intended_device_family => g_DEVICE_FAMILY--,
     )
     port map (
-        data => i_wdata,
-        wrreq => i_we,
-        wrfull => o_wfull,
-        wrusedw => o_wusedw,
-        wrclk => i_wclk,
-
         q => fifo_rdata,
-        rdreq => fifo_re,
+        rdreq => fifo_rack,
         rdempty => fifo_rempty,
         rdusedw => o_rusedw,
         rdclk => i_rclk,
+
+        data => fifo_wdata,
+        wrreq => fifo_we,
+        wrfull => fifo_wfull,
+        wrusedw => o_wusedw,
+        wrclk => i_wclk,
 
         -- Assert this signal to clear all the output status ports.
         -- There are no minimum number of clock cycles for aclr signals that must remain active.
         aclr => not i_reset_n--,
     );
 
-    e_reset_n : entity work.reset_sync
-    port map ( o_reset_n => reset_n, i_reset_n => i_reset_n, i_clk => i_rclk );
+    generate_showahead_off : if ( g_SHOWAHEAD /= "ON" ) generate
+        o_rdata <= fifo_rdata;
+        fifo_rack <= i_rack;
+        o_rempty <= fifo_rempty;
+    end generate;
 
-    e_fifo_rreg : entity work.fifo_rreg
+    generate_showahead_on : if ( g_SHOWAHEAD = "ON" ) generate
+        -- sync read clock reset
+        e_rreset_n : entity work.reset_sync
+        port map ( o_reset_n => rreset_n, i_reset_n => i_reset_n, i_clk => i_rclk );
+
+        -- read through reg fifo
+        e_fifo_rreg : entity work.fifo_reg
+        generic map (
+            g_DATA_WIDTH => g_DATA_WIDTH,
+            g_N => g_RREG_N--,
+        )
+        port map (
+            o_rdata => o_rdata,
+            i_rack => i_rack,
+            o_rempty => o_rempty,
+
+            i_wdata => fifo_rdata,
+            i_we => not fifo_rempty,
+            o_wfull_n => fifo_rack,
+
+            i_reset_n => rreset_n,
+            i_clk => i_rclk--,
+        );
+    end generate;
+
+    -- sync write clock reset
+    e_wreset_n : entity work.reset_sync
+    port map ( o_reset_n => wreset_n, i_reset_n => i_reset_n, i_clk => i_wclk );
+
+    -- write through reg fifo
+    e_fifo_wreg : entity work.fifo_reg
     generic map (
-        g_DATA_WIDTH => o_rdata'length--,
+        g_DATA_WIDTH => g_DATA_WIDTH,
+        g_N => g_WREG_N--,
     )
     port map (
-        o_rdata => o_rdata,
-        i_re => i_re,
-        o_rempty => o_rempty,
+        o_rdata => fifo_wdata,
+        i_rack => not fifo_wfull,
+        o_rempty_n => fifo_we,
 
-        i_fifo_rdata => fifo_rdata,
-        o_fifo_re => fifo_re,
-        i_fifo_rempty => fifo_rempty,
+        i_wdata => i_wdata,
+        i_we => i_we,
+        o_wfull => o_wfull,
 
-        i_reset_n => reset_n,
-        i_clk => i_rclk--,
+        i_reset_n => wreset_n,
+        i_clk => i_wclk--,
     );
 
 end architecture;
