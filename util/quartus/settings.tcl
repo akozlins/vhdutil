5cde16b54d084db3c5fcc379a77c0a119320d7e3
Alexandr Kozlinskiy <akozlins@gmail.com>
2024-03-07 11:43:56 +0100

    [util] update settings
    - disable auto shift regs

--- util/quartus/settings.tcl
+++ util/quartus/settings.tcl
@@ -1,12 +1,18 @@
-#
+# settings
 
 set_global_assignment -name PROJECT_IP_REGENERATION_POLICY NEVER_REGENERATE_IP
+# -> Assignments / Settings / Advanced Settings
 
-# Assignments / Settings / Advanced Settings
-
-# Tells the compiler to implement state machines that can recover gracefully from an illegal state.
+# implement state machines that can recover gracefully from an illegal state
 set_global_assignment -name SAFE_STATE_MACHINE ON
+# don't replace shift registers with altshift_taps megafunction (see Quartus User Guide / Auto Shift Register Replacement)
+# (<>)
+set_global_assignment -name AUTO_SHIFT_REGISTER_RECOGNITION OFF
 
-# Assignments / Settings / VHDL Input
+# -> Assignments / Settings / VHDL Input
 
 set_global_assignment -name VHDL_INPUT_VERSION VHDL_2008
+
+set_global_assignment -name SAVE_DISK_SPACE OFF
+
+##
