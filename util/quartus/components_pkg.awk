f9f85959ef08d36eceb3449e8f0b96524bdbd7c2
Alexandr Kozlinskiy <akozlins@gmail.com>
2024-03-07 11:49:21 +0100

    [util] update components package

--- util/quartus/components_pkg.awk
+++ util/quartus/components_pkg.awk
@@ -1,4 +1,4 @@
-#!/bin/awk -f
+#!/usr/bin/awk -f
 
 $1 == "component" && $3 == "is" && n[$2]++ {
     print "ERROR ["FILENAME":"NR"] " $2 " is already declared"
--- util/quartus/components_pkg.sh
+++ util/quartus/components_pkg.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -euf
 
-# find '*.cmp' files
-# and generate 'components' package
+# find `*.cmp` files
+# and generate `components` package
 
 if [ $# -ge 1 ] ; then
     cd -- "$1" || exit 1
@@ -16,11 +16,12 @@ package components is
 
     constant GIT_HEAD : std_logic_vector(16*4-1 downto 0) := X"$(git rev-parse --short=16 HEAD | rev)";
 
-EOF
-
-find -L . -name '*.cmp' -exec echo "--" '{}' ';' -exec cat '{}' ';'
-
-cat << EOF
+$(
+while read -r fname ; do
+    echo "-- $fname"
+    cat -- "$fname"
+done < <(find -L . -name '*.cmp' | sort)
+)
 
 end package;
 EOF
